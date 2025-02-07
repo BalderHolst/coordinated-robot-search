e736300858ab2f12444e211413fe1b1b02d4281b
Balder W. Holst <balderwh@gmail.com>
2025-02-07 12:32:41 +0100

    flake: Use native ros2 in devshell

--- flake.nix
+++ flake.nix
@@ -5,96 +5,8 @@
         nixpkgs.follows = "nix-ros-overlay/nixpkgs";
     };
     outputs = { flake-utils, nixpkgs, ... }@inputs:
-        let
-            container-name = "intelligent-systems";
-            dockerfile = ./Dockerfile;
-        in
         flake-utils.lib.eachDefaultSystem (system:
             let
-                commands = {
-                    build = /*bash*/ ''
-                        # Build if container does not exist
-                        if [ ! "$(docker ps -aq -f name=$CONTAINER)" ]; then
-                        echo "Container '$CONTAINER' does not exist. Building..."
-                        docker build -t $CONTAINER -f ${dockerfile} .
-                        fi
-                    '';
-                    start = /*bash*/ ''
-                        build
-                        # Start if container is stopped
-                        if [ "$(docker ps -aq -f status=exited -f name=$CONTAINER)" ]; then
-                                echo "Starting container..."
-                                docker start $CONTAINER > /dev/null
-                        fi
-                    '';
-                    stop = /*bash*/  ''
-                        # Stop if container is running
-                        if [ "$(docker ps -aq -f status=running -f name=$CONTAINER)" ]; then
-                                echo "Stopping container..."
-                                docker stop $CONTAINER > /dev/null
-                        else
-                                echo "Container is not running"
-                        fi
-                    '';
-                    run = /*bash*/ ''
-                        # Make sure a command was provided
-                        if [ -z "$1" ]; then
-                                echo "No command provided"
-                                exit 1
-                        fi
-
-                        build
-                        start
-
-                        cmd="$@"
-
-                        docker exec -it \
-                            --privileged \
-                            --workdir="/root/ws" \
-                            $CONTAINER bash -i -c "$cmd"
-                    '';
-                    enter = /*bash*/ ''
-                            build
-                            start
-
-                            XAUTH=/tmp/.docker.xauth
-
-                            [ "$(docker ps | grep $CONTAINER)" ] && {
-                                    echo "Attaching to running container..."
-                                    docker exec -it \
-                                        --privileged \
-                                        --workdir="/root/ws" \
-                                        $CONTAINER bash
-                                    exit 0
-                            }
-
-                            echo "Running container..."
-                            docker run -it \
-                                    --name $CONTAINER \
-                                    --env="DISPLAY=$DISPLAY" \
-                                    --env="QT_X11_NO_MITSHM=1" \
-                                    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-                                    --env="XAUTHORITY=$XAUTH" \
-                                    --volume="$XAUTH:$XAUTH" \
-                                    --net=host \
-                                    --workdir="/root/ws" \
-                                    --volume="$ROOT:/root/ws" \
-                                    --privileged \
-                                    $CONTAINER \
-                                    bash
-                    '';
-                    delete = /*bash*/ ''
-                        echo "Deleting container '$CONTAINER'..."
-                        docker rm $CONTAINER > /dev/null
-                    '';
-                    help = /*bash*/ ''
-                        echo -e "\nAvailable Commands:"
-                        ${ lib.concatStringsSep "\n" (
-                            lib.attrsets.mapAttrsToList (k: v: "echo -e '\t${k}'") commands
-                        )}
-                        echo ""
-                    '';
-                };
                 pkgs = import nixpkgs {
                     inherit system;
                     overlays = [ inputs.nix-ros-overlay.overlays.default ];
@@ -102,24 +14,17 @@
                 lib = nixpkgs.lib;
             in {
         devShells.default = pkgs.mkShell {
-            name = "Docker based development environment";
-
-            packages = with pkgs; with pkgs.python3Packages; [
+            packages = with pkgs; [
                 git
                 docker
-                python3
-                # rosPackages.humble.rclpy
-
-            ] ++ lib.attrsets.mapAttrsToList
-                (k: v: pkgs.writeShellScriptBin k v)
-                commands;
-
-            shellHook = ''
-                ${commands.help}
-
-                export ROOT="$(pwd)"
-                export CONTAINER="${container-name}"
-            '';
+                colcon
+                (with rosPackages.humble; buildEnv {
+                    paths = [
+                        ros-core
+                        rviz2
+                    ];
+                })
+            ];
         };
     });
     nixConfig = {
