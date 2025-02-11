62f70a1a449c5d3917f414e4df0e185e78927af9
MathiasBalling <math260r@gmail.com>
2025-02-11 22:29:13 +0100

    Working r2r pub/sub for /map

--- ros2/test_r2r_ws/src/map_to_grid/src/main.rs
+++ ros2/test_r2r_ws/src/map_to_grid/src/main.rs
@@ -1,33 +1,54 @@
-use futures::{executor::LocalPool, future, stream::StreamExt, task::LocalSpawnExt};
-use r2r::nav_msgs::msg::OccupancyGrid;
+use futures::{executor::LocalPool, stream::StreamExt, task::LocalSpawnExt};
 use r2r::QosProfile;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    r2r::log_info!("before_init", "debug msg");
     let ctx = r2r::Context::create()?;
-    let mut node = r2r::Node::create(ctx, "map2grid", "")?;
-    let subscriber = node.subscribe::<OccupancyGrid>("/map", QosProfile::default())?;
+    let mut node = r2r::Node::create(ctx, "echolist", "")?;
+    let qos = QosProfile::default()
+        .reliable()
+        .transient_local()
+        .keep_all();
+    let mut sub = node.subscribe::<r2r::nav_msgs::msg::OccupancyGrid>("/map", qos.clone())?;
+    // let mut p = node.create_publisher::<r2r::nav_msgs::msg::OccupancyGrid>("/map", qos)?;
+    let nl = node.logger().to_owned();
 
-    println!("node name: {}", node.name()?);
-    println!(
-        "node fully qualified name: {}",
-        node.fully_qualified_name()?
-    );
-    // Set up a simple task executor.
     let mut pool = LocalPool::new();
     let spawner = pool.spawner();
 
-    // Run the subscriber in one task, printing the messages
+    // task that every other time forwards message to topic2
     spawner.spawn_local(async move {
-        subscriber
-            .for_each(|msg| {
-                println!("got new msg: {:?}", msg.data);
-                future::ready(())
-            })
-            .await
+        println!("Echo list node started");
+        loop {
+            // Code to publish to a topic
+            // let send = r2r::nav_msgs::msg::OccupancyGrid {
+            //     info: r2r::nav_msgs::msg::MapMetaData {
+            //         resolution: 5.25,
+            //         ..Default::default()
+            //     },
+            //     ..Default::default()
+            // };
+            // p.publish(&send).unwrap();
+            if let Some(msg) = sub.next().await {
+                r2r::log_info!(
+                    &nl,
+                    "Map info: {},{}:{}",
+                    msg.info.height,
+                    msg.info.width,
+                    msg.info.resolution
+                );
+                r2r::log_debug!(&nl, "test");
+                r2r::log_info!(&nl, "test");
+                r2r::log_warn!(&nl, "test");
+                r2r::log_error!(&nl, "test");
+                r2r::log_fatal!(&nl, "test");
+            } else {
+                r2r::log_fatal!(&nl, "broken");
+                break;
+            }
+        }
     })?;
 
-    // Main loop spins ros.
     loop {
         node.spin_once(std::time::Duration::from_millis(100));
         pool.run_until_stalled();
