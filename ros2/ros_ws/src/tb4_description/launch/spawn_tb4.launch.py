717ceb769fefed03db1a7ea9f3d8166553985037
MathiasBalling <math260r@gmail.com>
2025-02-27 17:13:29 +0100

    All topics working with ros_gz_bridge

--- ros2/ros_ws/src/tb4_description/launch/spawn_tb4.launch.py
+++ ros2/ros_ws/src/tb4_description/launch/spawn_tb4.launch.py
@@ -48,23 +48,9 @@ def generate_launch_description():
         "robot_name", default_value="turtlebot4", description="name of the robot"
     )
 
-    robot_bridge = os.path.join(sim_dir, "launch", "bridge_robot_tmp.yaml")
-    camera_bridge_image = os.path.join(
-        sim_dir, "launch", "bridge_camera_image_tmp.yaml"
-    )
-    camera_bridge_depth = os.path.join(
-        sim_dir, "launch", "bridge_camera_depth_tmp.yaml"
-    )
+    robot_bridge = os.path.join(sim_dir, "params", "bridge_robot_tmp.yaml")
 
     robot_bridge_namespaced = ReplaceString(
-        source_file=robot_bridge,
-        replacements={"<gz_namespace>": ("/", namespace)},
-    )
-    camera_bridge_image_namespaced = ReplaceString(
-        source_file=camera_bridge_image,
-        replacements={"<gz_namespace>": ("/", namespace)},
-    )
-    camera_bridge_depth_namespaced = ReplaceString(
         source_file=robot_bridge,
         replacements={"<gz_namespace>": ("/", namespace)},
     )
@@ -81,35 +67,6 @@ def generate_launch_description():
             }
         ],
         output="screen",
-    )
-
-    camera_bridge_image = Node(
-        package="ros_gz_image",
-        executable="image_bridge",
-        name="bridge_gz_ros_camera_image",
-        namespace=namespace,
-        output="screen",
-        parameters=[
-            {
-                "config_file": camera_bridge_image_namespaced,
-                "use_sim_time": use_sim_time,
-            }
-        ],
-    )
-
-    camera_bridge_depth = Node(
-        package="ros_gz_image",
-        executable="image_bridge",
-        name="bridge_gz_ros_camera_depth",
-        namespace=namespace,
-        output="screen",
-        parameters=[
-            {
-                "config_file": camera_bridge_depth_namespaced,
-                "use_sim_time": use_sim_time,
-            }
-        ],
-        arguments=["rgbd_camera/depth_image"],
     )
 
     spawn_model = Node(
@@ -147,7 +104,5 @@ def generate_launch_description():
     ld.add_action(declare_use_sim_time_cmd)
 
     ld.add_action(bridge)
-    ld.add_action(camera_bridge_image)
-    ld.add_action(camera_bridge_depth)
     ld.add_action(spawn_model)
     return ld
