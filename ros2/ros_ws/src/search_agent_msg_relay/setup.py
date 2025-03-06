1eddf7dd83fbec5fe3b48de3c0c226e5fff583b8
Balder W. Holst <balderwh@gmail.com>
2025-03-06 22:42:15 +0100

    fixup! Robots can communicate!

--- ros2/ros_ws/src/search_agent_msg_relay/setup.py
+++ ros2/ros_ws/src/search_agent_msg_relay/setup.py
@@ -5,7 +5,7 @@ package_name = 'search_agent_msg_relay'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[],
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
