6dfce8feed378960c55f97990a4d31cbcbc00439
MathiasBalling <math260r@gmail.com>
2025-03-24 12:24:16 +0100

    fix(botbrain): Match tb4 params

--- botbrain/src/params.rs
+++ botbrain/src/params.rs
@@ -2,7 +2,7 @@ use std::f32::consts::PI;
 
 pub const DIAMETER: f32 = 0.5;
 pub const RADIUS: f32 = DIAMETER / 2.0;
-pub const CAM_RANGE: f32 = 3.0;
-pub const CAM_FOV: f32 = PI / 2.0;
-pub const LIDAR_RANGE: f32 = 5.0;
+pub const CAM_RANGE: f32 = 3.0; // TODO: Probably should be higher
+pub const CAM_FOV: f32 = 1.25;
+pub const LIDAR_RANGE: f32 = 20.0;
 pub const COMMUNICATION_RANGE: f32 = 30.0;
