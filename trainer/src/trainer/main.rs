7d91a8d24f48a63feec22ce7ba3b113414682368
BalderHolst <balderwh@gmail.com>
2025-04-12 11:08:06 +0200

    rl: Create `worldgen` executable

--- trainer/src/trainer/main.rs
+++ trainer/src/trainer/main.rs
@@ -67,7 +67,7 @@ struct Cli {
     #[arg(long, default_value_t = 0.05)]
     eps_end: f64,
 
-    #[arg(short, long, default_value_t = 4)]
+    #[arg(long, default_value_t = 4)]
     max_robots: usize,
 }
 
