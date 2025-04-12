708ad8f37d29f06e1246cf0e76723fb0812c3536
Mathias Balling <math260r@gmail.com>
2025-04-12 14:45:29 +0200

    feat(botbrain): Structure for frontiers exploration is ready

--- botbrain/src/behaviors/search/frontiers.rs
+++ botbrain/src/behaviors/search/frontiers.rs
@@ -1,20 +1,15 @@
+#![allow(unused)]
+
+use std::collections::HashSet;
+
 use emath::Pos2;
 
-use crate::{behaviors::ScaledGrid, grid::Grid};
+use crate::behaviors::ScaledGrid;
 
 use super::{
     costmap::{COSTMAP_SEARCHED, COSTMAP_UNKNOWN},
     pathing::NEIGHBORS,
 };
-
-// TODO: Find frontier regions
-pub fn evaluate_frontiers(robot_pos: Pos2, costmap_grid: &ScaledGrid<f32>) -> Pos2 {
-    let robot_pos = {
-        let tmp = costmap_grid.world_to_grid(robot_pos);
-        (tmp.x as usize, tmp.y as usize)
-    };
-    Pos2::new(25.0, -20.0)
-}
 
 /// Frontier is a known cell with unknown neighbors
 /// pos is the position of the cell in the underlying grid of the ScaledGrid
@@ -42,15 +37,6 @@ pub fn is_frontier(pos: (usize, usize), costmap_grid: &ScaledGrid<f32>) -> bool 
     })
 }
 
-// TODO: Choose the frontier region to explore
-fn evaluate_frontier_regions(
-    robot_pos: Pos2,
-    frontiers: Grid<FrontierRegion>,
-    costmap_grid: &ScaledGrid<f32>,
-) -> Pos2 {
-    todo!()
-}
-
 #[derive(Clone, Copy, Debug, PartialEq, Default)]
 enum FrontierRegion {
     #[default]
@@ -58,11 +44,31 @@ enum FrontierRegion {
     Id(usize),
 }
 
-// TODO: Make a grid of the frontier regions
-fn make_frontier_region(
-    robot_pos: Pos2,
-    frontiers: Pos2,
-    costmap_grid: &ScaledGrid<f32>,
-) -> Grid<FrontierRegion> {
+fn find_frontiers(robot_pos: Pos2, costmap_grid: &ScaledGrid<f32>) -> HashSet<(usize, usize)> {
+    // TODO: Start from the robot position and find all frontiers
+    // Use BFS to find all frontiers?
     todo!()
+}
+
+fn make_frontier_regions(
+    robot_pos: Pos2,
+    frontiers: HashSet<(usize, usize)>,
+    costmap_grid: &ScaledGrid<f32>,
+) -> ScaledGrid<FrontierRegion> {
+    // TODO: Group the frontiers into regions
+    // Using region growing?
+    todo!()
+}
+
+fn evaluate_frontier_regions(robot_pos: Pos2, frontiers: ScaledGrid<FrontierRegion>) -> Pos2 {
+    // TODO: Choose the frontier region to explore
+    // Could be a weighted based on the size of the region and distance to the robot?
+    todo!()
+}
+
+pub fn evaluate_frontiers(robot_pos: Pos2, costmap_grid: &ScaledGrid<f32>) -> Pos2 {
+    // let frontiers = find_frontiers(robot_pos, costmap_grid);
+    // let frontier_regions = make_frontier_regions(robot_pos, frontiers, costmap_grid);
+    // let goal = evaluate_frontier_regions(robot_pos, frontier_regions);
+    Pos2::new(25.0, -20.0)
 }
