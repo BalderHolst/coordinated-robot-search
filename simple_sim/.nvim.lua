8b59f109563848b063d27aaa4fdac8f8366c1f24
Mathias Balling <math260r@gmail.com>
2025-04-15 15:59:34 +0200

    nvim: Use builtin 'exrc' for project lsp settings

--- simple_sim/.nvim.lua
+++ simple_sim/.nvim.lua
@@ -1,15 +1,21 @@
--- run ```:luafile .nvim.lua``` after startup to load this file
--- This uses https://github.com/mrcjkb/rustaceanvim
--- Normally lsp-config is used.
+-- run `:luafile .nvim.lua` after startup to load this file
+-- Or auto load by using 'exrc' (vim.o.exrc = true)
+-- This uses https://github.com/mrcjkb/rustaceanvim or vim.lsp
 
 local config = {
-	cargo = {
-		allFeatures = false,
-		features = { "default" },
+	["rust-analyzer"] = {
+		cargo = {
+			allFeatures = false,
+			features = { "default" },
+		},
 	},
 }
 
-local config_str = vim.inspect(config)
+-- For rustaceanvim
+local rustaceanvim_default_config = vim.g.rustaceanvim or {}
+vim.g.rustaceanvim =
+	vim.tbl_deep_extend("force", rustaceanvim_default_config, { server = { default_settings = config } })
 
-vim.cmd.RustAnalyzer({ "config", config_str })
-vim.cmd.RustAnalyzer({ "restart" })
+-- For vim.lsp
+-- local lsp_default_config = vim.lsp.config["rust-analyzer"] or {}
+-- vim.lsp.config["rust-analyzer"] = vim.tbl_deep_extend("force", lsp_default_config, { settings = config })
