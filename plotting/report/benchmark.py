83f13f1ea250bedcade4baf45001243d5f698427
Balder W. Holst <balderwh@gmail.com>
2025-05-21 23:37:45 +0200

    plotting: Mode extensive benchmark

--- plotting/report/benchmark.py
+++ plotting/report/benchmark.py
@@ -6,7 +6,7 @@ WORLD = bp.repo_path("worlds/objectmap/pathing_example.ron")
 
 RUNS = 10
 DURATION = 800
-ROBOTS = 4
+ROBOTS = [i+1 for i in range(6)]
 
 BEHAVIORS = [
     ("avoid-obstacles", "Roomba"),
@@ -18,45 +18,46 @@ BEHAVIORS = [
 DIR = bp.repo_path("report", "figures", "plots", "benchmarks")
 
 def main():
-
-    collections = []
-
-    for behavior, name in BEHAVIORS:
-        results: list[bp.Result] = []
-
-        bp.seed(42)
-        for i in range(RUNS):
-            print(f"{name} run {i+1}/{RUNS}")
-
-            scenario = bp.Scenario(
-                title=f"{name} run {i+1}",
-                world=WORLD,
-                behavior=behavior,
-                duration=DURATION,
-                robots=ROBOTS,
-            )
-
-            res = bp.run_sim(scenario, f"multiple_runs/run-{i+1}")
-            results.append(res)
-
-        collections.append(bp.ResultCollection(name, results))
-
     plot_files = []
 
-    plot_files.append(
-        bp.plot_coverage(collections, f"Coverage over {RUNS} runs")
-    )
+    for robots in ROBOTS:
 
-    plot_files.append(
-        bp.plot_spread(collections, f"Spread over {RUNS} runs")
-    )
+        collections = []
+
+        for behavior, name in BEHAVIORS:
+            results: list[bp.Result] = []
+
+            bp.seed(42)
+            for i in range(RUNS):
+                print(f"\n========== [{i+1}/{RUNS}] {name} with {robots} robots ==========")
+
+                scenario = bp.Scenario(
+                    title=f"{name} run {i+1}",
+                    world=WORLD,
+                    behavior=behavior,
+                    duration=DURATION,
+                    robots=robots,
+                )
+
+                res = bp.run_sim(scenario, f"multiple_runs/run-{i+1}")
+                results.append(res)
+
+            collections.append(bp.ResultCollection(f"{name} with {robots} robots", results))
+
+
+        plot_files.append(
+            bp.plot_coverage(collections, f"Coverage over {RUNS} runs")
+        )
+
+        plot_files.append(
+            bp.plot_spread(collections, f"Spread over {RUNS} runs")
+        )
 
     for plot_file in plot_files:
         dst = os.path.join(DIR, os.path.basename(plot_file))
         os.makedirs(os.path.dirname(dst), exist_ok=True)
         shutil.copyfile(plot_file, dst)
-        print(f"Plot copied to '{dst}'")
-
+        print(f"Plot copied to '{bp.relpath(dst)}'")
 
 
 
