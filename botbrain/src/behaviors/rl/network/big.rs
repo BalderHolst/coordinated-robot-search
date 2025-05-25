f38d90700fcd1acae1f708370ad3581b4b5e26c6
Balder W. Holst <balderwh@gmail.com>
2025-05-25 12:27:55 +0200

    botbrain: Add missing files

---
+++ botbrain/src/behaviors/rl/network/big.rs
@@ -0,0 +1,58 @@
+//! Implementation of [`LargeNetwork`].
+
+use burn::{
+    nn::{Linear, LinearConfig},
+    prelude::*,
+    tensor::{activation, backend::AutodiffBackend},
+};
+
+use crate::behaviors::rl::{action::Action, state::State};
+
+use super::{soft_update_linear, AutodiffNetwork, Network};
+
+/// A small neural network with one hidden layer.
+#[derive(Debug, Module)]
+pub struct LargeNetwork<B: Backend> {
+    linear1: Linear<B>,
+    linear2: Linear<B>,
+    linear3: Linear<B>,
+    linear4: Linear<B>,
+    linear5: Linear<B>,
+}
+
+impl<B: Backend> LargeNetwork<B> {
+    const HIDDEN_SIZE_1: usize = 30;
+    const HIDDEN_SIZE_2: usize = 40;
+    const HIDDEN_SIZE_3: usize = 30;
+    const HIDDEN_SIZE_4: usize = 20;
+}
+
+impl<B: AutodiffBackend, S: State, A: Action> AutodiffNetwork<B, S, A> for LargeNetwork<B> {}
+
+impl<B: Backend, S: State, A: Action> Network<B, S, A> for LargeNetwork<B> {
+    fn init(device: &B::Device) -> Self {
+        Self {
+            linear1: LinearConfig::new(S::SIZE, Self::HIDDEN_SIZE_1).init(device),
+            linear2: LinearConfig::new(Self::HIDDEN_SIZE_1, Self::HIDDEN_SIZE_2).init(device),
+            linear3: LinearConfig::new(Self::HIDDEN_SIZE_2, A::SIZE).init(device),
+            linear4: LinearConfig::new(Self::HIDDEN_SIZE_3, A::SIZE).init(device),
+            linear5: LinearConfig::new(Self::HIDDEN_SIZE_4, A::SIZE).init(device),
+        }
+    }
+
+    fn forward(&self, input: Tensor<B, 2>) -> Tensor<B, 2> {
+        let x = activation::sigmoid(self.linear1.forward(input));
+        let x = activation::sigmoid(self.linear2.forward(x));
+        activation::sigmoid(self.linear3.forward(x))
+    }
+
+    fn soft_update(this: Self, that: &Self, tau: f64) -> Self {
+        Self {
+            linear1: soft_update_linear(this.linear1, &that.linear1, tau),
+            linear2: soft_update_linear(this.linear2, &that.linear2, tau),
+            linear3: soft_update_linear(this.linear3, &that.linear3, tau),
+            linear4: soft_update_linear(this.linear4, &that.linear4, tau),
+            linear5: soft_update_linear(this.linear5, &that.linear5, tau),
+        }
+    }
+}
---
+++ botbrain/src/behaviors/rl/robots/bigboy.rs
@@ -0,0 +1,45 @@
+//! Implementation of [`BigBoyRlRobot`].
+
+use std::time::Duration;
+
+use crate::behaviors::rl::network;
+use crate::{behaviors::BehaviorOutput, Robot, RobotRef};
+
+use crate::behaviors::rl::{action, state, RlRobot};
+
+use super::run_robot;
+
+use burn::prelude::*;
+
+type St = state::PolarState;
+type Ac = action::SliderAction<9>;
+type Net<B> = network::large::LargeNetwork<B>;
+
+/// A reinforcement learning search robot using polar coordinates for its state
+pub type BigBoyRlRobot<B> = RlRobot<B, St, Ac, Net<B>>;
+
+/// Behavior function for the polar RL robot
+pub fn run<B: Backend>(robot: &mut RobotRef, time: Duration) -> BehaviorOutput {
+    run_robot(robot, time, |robot: &mut BigBoyRlRobot<B>| {
+        robot.visualize_state();
+    })
+}
+
+// impl<B: Backend> network::TrainedNetwork<B, St, Ac> for Net<B> {
+//     fn bytes() -> &'static [u8] {
+//         include_bytes!("weights/bigboy.bin")
+//     }
+// }
+
+impl<B: Backend> RlRobot<B, St, Ac, Net<B>> {
+    fn visualize_state(&mut self) {
+        if !self.debug_enabled() {
+            return;
+        }
+
+        let state = self.state();
+        let soup = &mut self.debug_soup;
+
+        state.visualize(soup);
+    }
+}
